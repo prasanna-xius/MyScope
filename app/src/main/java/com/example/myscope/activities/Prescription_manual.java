b96d936f1686bc2a33c58e604fb2d0ab459e97e4
prasanna <58811884+prasanna-xius@users.noreply.github.com>
2019-12-23 17:42:32 +0530

    manual prescription

---
+++ app/src/main/java/com/example/myscope/activities/Prescription_manual.java
@@ -0,0 +1,16 @@
+package com.example.myscope.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myscope.R;
+
+public class Prescription_manual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescription_manual);
+    }
+}
