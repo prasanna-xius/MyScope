0e7809f0658e26cdd3e3f1c3447a5d8a040556f7
saisankeerthb <59042353+saisankeerthb@users.noreply.github.com>
2020-01-22 10:59:21 +0530

    medical history , family history, social history, diet.
    working on validations

---
+++ app/src/main/java/com/example/myscope/activities/Diet.kt
@@ -0,0 +1,22 @@
+package com.example.myscope.activities
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.myscope.R
+
+class Diet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.diet)
+        var spinner_diet = findViewById(R.id.spinner_diet) as Spinner
+        val adapter = ArrayAdapter.createFromResource(this, R.array.diet_arrays, R.layout.spinner_item)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+
+        spinner_diet.setAdapter(adapter)
+
+    }
+}
