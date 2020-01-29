9fdd4e8f986efe61c5acb08c6c2e1053a92ce9a0
saisankeerthb <59042353+saisankeerthb@users.noreply.github.com>
2020-01-29 11:22:38 +0530

    changes medicalhistory

--- app/src/main/java/com/example/myscope/activities/medical_history/Diet.kt
+++ app/src/main/java/com/example/myscope/activities/medical_history/Diet.kt
@@ -2,21 +2,42 @@ package com.example.myscope.activities.medical_history
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import android.widget.Spinner
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.myscope.R
+import com.example.myscope.activities.BaseActivity
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.diet.*
+import kotlinx.android.synthetic.main.spinner_dropdown_item.view.*
 
-class Diet : AppCompatActivity() {
+class Diet : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diet)
-        var spinner_diet = findViewById(R.id.spinner_diet) as Spinner
-        val adapter = ArrayAdapter.createFromResource(this, R.array.diet_arrays, R.layout.spinner_item)
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        showToolbar()
+        header!!.text = "Diet"
 
-        spinner_diet.setAdapter(adapter)
+//        var spinner_diet = findViewById(R.id.spinner_diet) as Spinner
+//        val adapter = ArrayAdapter.createFromResource(this, R.array.diet_arrays, R.layout.spinner_item)
+//        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+//        spinner_diet.setAdapter(adapter)
+
+        spinnerSet(spinner_diet,resources.getStringArray(R.array.diet_arrays))
+
+        btn_diet.setOnClickListener {
+
+            if (spinner_diet.selectedItem.equals("Select status")){
+
+                Toast.makeText(applicationContext,"cmg",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(applicationContext,"wfgeug",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        }
 
     }
-}
--- app/src/main/java/com/example/myscope/activities/medical_history/Medical_History.kt
+++ app/src/main/java/com/example/myscope/activities/medical_history/Medical_History.kt
@@ -1,27 +1,66 @@
 package com.example.myscope.activities.medical_history
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-
-import androidx.appcompat.app.AppCompatActivity
-
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
 import com.example.myscope.R
+import com.example.myscope.activities.BaseActivity
+import kotlinx.android.synthetic.main.custom_spinner.*
 import kotlinx.android.synthetic.main.medical_history.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class Medical_History : AppCompatActivity() {
+class Medical_History : BaseActivity() {
+
+    internal lateinit var myCalendar: Calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.medical_history)
-        var edit_text1=findViewById<EditText>(R.id.edit_text1)
 
-        var spinner_disease = findViewById(R.id.spinner_disease) as Spinner
         val adapter = ArrayAdapter.createFromResource(this, R.array.disease_arrays, R.layout.spinner_item)
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-
         spinner_disease.setAdapter(adapter)
+
+        errorRemove(condition)
+        spinnerSet(spinner_disease,resources.getStringArray(R.array.disease_arrays))
+
+
+        myCalendar = Calendar.getInstance()
+        val date = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, monthOfYear)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
         }
 
-    }
+        et_startDate.setOnClickListener {
+            // TODO Auto-generated method stub
+            DatePickerDialog(this, R.style.MyDatePicker, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        val layoutInflater: LayoutInflater = LayoutInflater.from(applicationContext)
+
+        // Inflate the layout using LayoutInflater
+        val view: View = layoutInflater.inflate(
+                R.layout.custom_spinner,
+                linearlayout, // Root layout to attach the view
+                false
+        )
+        val label = view.findViewById<TextView>(R.id.textBox)
+//         label.setText(howOftenTaken)
+
+//        val timpicker = view.findViewById<TimePicker>(R.id.customtimepicker)
+//        val checkbox = view.findViewById<View>(R.id.checkbox)
+        }
+    private fun updateLabel() {
+        val myFormat = "MM/dd/yyyy" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        et_startDate.text = sdf.format(myCalendar.time)
+        }
+
+
+}
