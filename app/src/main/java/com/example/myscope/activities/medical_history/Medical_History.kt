7ed16f2b22762e4c72d44768dac2aa360716d3c2
saisankeerthb <59042353+saisankeerthb@users.noreply.github.com>
2020-01-29 13:10:28 +0530

    Medical History
    medical history changed

---
+++ app/src/main/java/com/example/myscope/activities/medical_history/Medical_History.kt
@@ -0,0 +1,82 @@
+package com.example.myscope.activities.medical_history
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import com.example.myscope.R
+import com.example.myscope.activities.BaseActivity
+import kotlinx.android.synthetic.main.activity_prescription_manual.*
+import kotlinx.android.synthetic.main.calander_layout.*
+import kotlinx.android.synthetic.main.custom_spinner.*
+import kotlinx.android.synthetic.main.medical_history.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Medical_History : BaseActivity() {
+
+    internal lateinit var myCalendar: Calendar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.medical_history)
+
+//        val adapter = ArrayAdapter.createFromResource(this, R.array.disease_arrays, R.layout.spinner_item)
+//        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+//        spinner_disease.setAdapter(adapter)
+
+        errorRemove(condition)
+        spinnerSet(spinner_disease,resources.getStringArray(R.array.disease_arrays))
+
+
+
+        bt_medicalHistory.setOnClickListener {
+            if (condition.text.toString().trim().equals("")) {
+                errorDisplay(condition)
+            }
+        }
+
+        myCalendar = Calendar.getInstance()
+        val date = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, monthOfYear)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        }
+
+
+
+        et_startDate.setOnClickListener {
+            // TODO Auto-generated method stub
+            DatePickerDialog(this, R.style.MyDatePicker, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        val layoutInflater: LayoutInflater = LayoutInflater.from(applicationContext)
+
+        // Inflate the layout using LayoutInflater
+        val view: View = layoutInflater.inflate(
+                R.layout.custom_spinner,
+                linearlayout, // Root layout to attach the view
+                false
+        )
+        val label = view.findViewById<TextView>(R.id.textBox)
+//        label.setText(spinner_disease)
+//        val timpicker = view.findViewById<TimePicker>(R.id.customtimepicker)
+//        val checkbox = view.findViewById<View>(R.id.checkbox)
+
+
+
+        }
+
+    private fun updateLabel() {
+        val myFormat = "MM/dd/yyyy" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        et_startDate.text = sdf.format(myCalendar.time)
+
+        }
+
+
+
+}
