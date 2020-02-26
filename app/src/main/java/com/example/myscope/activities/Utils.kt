9efbbe080952f51097655e1df1d26235266a5313
karthik-xius <58927993+karthik-xius@users.noreply.github.com>
2020-02-26 11:17:15 +0530

    dashboard changes
    dashboard changes

--- app/src/main/java/com/example/myscope/activities/Utils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myscope.activities
-
-object Utils {
-      
-       val BASE_URL = "Add your backend end point"
-
-       val apiService: APIService
-       get() = RetrofitClient1.getClient(BASE_URL)!!.create(APIService::class.java)
-}
