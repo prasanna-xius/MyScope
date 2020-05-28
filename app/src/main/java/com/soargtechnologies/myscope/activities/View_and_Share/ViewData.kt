3ef19a6ac2dc4350f5212dd93a06b00da28ad5fc
prasanna <58811884+prasanna-xius@users.noreply.github.com>
2020-05-28 14:48:11 +0530

    view data page

---
+++ app/src/main/java/com/soargtechnologies/myscope/activities/View_and_Share/ViewData.kt
@@ -0,0 +1,14 @@
+package com.soargtechnologies.myscope.activities.View_and_Share
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.soargtechnologies.myscope.R
+
+class ViewData : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_data)
+
+    }
+}
